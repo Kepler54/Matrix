45bf4adb659c10a0c34b453078507afc42153e52
Kepler <keplersemail@proton.me>
2023-05-08 15:17:48 +0200

    2. Module redesigned as a separate program

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import bext
-from os import system
-from random import randint
+from time import sleep
 from threading import Thread
 from matrix_for_linux import MatrixForLinux
 from matrix_for_windows import MatrixForWindows
@@ -9,8 +8,8 @@ matrix_linux = MatrixForLinux()
 matrix_windows = MatrixForWindows()
 
 if __name__ == '__main__':
-    system('cls')
-    Thread(target=matrix_windows.runner).start()
-    matrix_windows.matrixmove(-1, bext.height() - 1, float(f'{0.0}{randint(6, 9)}'))
-    Thread(target=matrix_windows.breaker).start()
-    input()
+    matrix_windows.title()
+    matrix_windows.fullscreen()
+    sleep(0.1)
+    matrix_windows.matrixmove(-1, bext.height() - 2)
+    Thread(target=matrix_windows.breaker()).start()
--- matrix_for_windows.py
+++ matrix_for_windows.py
@@ -1,11 +1,10 @@
 import bext
-from bext import goto
-from bext import hide
+import threading
 from time import sleep
 from colorama import Fore
-from threading import Lock
 from random import randint
 from threading import Thread
+from keyboard import press_and_release
 
 
 class MatrixForWindows:
@@ -16,25 +15,31 @@ class MatrixForWindows:
     z = ('Ё', 'У', 'р', 'Ф', 'q', 'ё', 'R', 'й', 'Ь', 'ю')
     o = (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ')
 
-    locker = Lock()
-    switch = False
+    locker = threading.Lock()
     maincounter = 0
-    horizcoordcounter = 1  # счетчик и начальная координата по горизонтали
+    stop = False
+    horizcoordcounter = 1  # counter and initial horizontal coordinate
 
-    '''Выключатель матрицы'''
+    '''Title'''
+
+    @staticmethod
+    def title():
+        return bext.title("Matrix, version 1.0")
+
+    '''Full screen mode'''
+
+    @staticmethod
+    def fullscreen():
+        return press_and_release('alt+enter')
+
+    '''Matrix switch'''
 
     @classmethod
     def breaker(cls):
         input()
-        cls.switch = True
+        cls.stop = True
 
-    '''Включатель матрицы'''
-
-    @classmethod
-    def runner(cls):
-        cls.switch = False
-
-    '''Формирующая случайный символ из картежа функция'''
+    '''Forming a random symbol from the map function'''
 
     def matrixsymbol(self):
         f = (
@@ -47,37 +52,35 @@ class MatrixForWindows:
         )
         return f[randint(0, 5)]
 
-    '''Функция формирует пустоту'''
+    '''The function forms a void'''
 
     def matrixvoid(self):
         f = (self.o[(randint(0, 9))], self.o[(randint(0, 9))])
         return f[randint(0, 1)]
 
-    '''Формирующая каплю функция'''
+    '''Drop shaping function'''
 
     def matrixdrop(self, dropheight, horizcoord, coordofthedropbeginn):
-        while not self.switch:  # общий цикл процесса
-            for b in range(6):  # цикл жизни капли
+        while not self.stop:  # overall process cycle
+            for b in range(6):  # drop life cycle
                 verticalcoordcounter = coordofthedropbeginn
-                self.maincounter += 1  # счетчик главного цикла
-                for c in range(randint(0, dropheight)):  # случайная координата конечной высоты капли
-                    verticalcoordcounter += 1  # в каждом новом цикле высота капли умножается на 1
+                self.maincounter += 1  # main cycle counter
+                for c in range(randint(0, dropheight)):  # random coordinate of the final drop height
+                    verticalcoordcounter += 1  # in each new cycle the drop height is multiplied by 1
                     with self.locker:
-                        goto(horizcoord, verticalcoordcounter)  # координаты ширины и высоты капли
-                        if self.maincounter % 3 == 0:  # если число главного цикла делится на 0
-                            print(f'{Fore.GREEN}{self.matrixvoid()}')  # выводится пустота
+                        bext.goto(horizcoord, verticalcoordcounter)  # coordinates of drop width and drop height
+                        if self.maincounter % 3 == 0:  # if the number of the main loop is divisible by 0
+                            print(f'{Fore.GREEN}{self.matrixvoid()}')  # a void is output
                         else:
-                            print(f'{Fore.GREEN}{self.matrixsymbol()}')  # в противном случае генерируется капелька
+                            print(f'{Fore.GREEN}{self.matrixsymbol()}')  # otherwise a droplet is generated
                     sleep(float(f'{0.0}{randint(3, 6)}'))
             self.maincounter = 0
 
-    '''Формирующая потоки капель функция'''
+    '''Shaping droplet streams function'''
 
-    def matrixmove(self, coordofthedropbeginn, dropheight, timesleep):
-        hide()
+    def matrixmove(self, coordofthedropbeginn, dropheight):
+        bext.hide()
         for q in range(bext.width() // 2):
-            Thread(
-                target=MatrixForWindows().matrixdrop,
-                args=(dropheight, self.horizcoordcounter, coordofthedropbeginn, timesleep)
-            ).start()
-            self.horizcoordcounter += 2  # шаг между потоками капель
+            Thread(target=MatrixForWindows().matrixdrop,
+                   args=(dropheight, self.horizcoordcounter, coordofthedropbeginn)).start()
+            self.horizcoordcounter += 2  # pitch between droplet streams
